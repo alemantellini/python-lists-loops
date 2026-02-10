fa0ff718c01446fa5622bb80edc536a4aedab826
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:19:48 +0100

    Print the last element of the random list
    Retrieve and print the last element of the generated list.

--- exercises/01.3-Print-the-last-one/app.py
+++ exercises/01.3-Print-the-last-one/app.py
@@ -1,16 +1,15 @@
+# EJERCICIO 1.3 - print the last one
 # import the random package here "import random"
 import random
-
 def generate_random_list():
     aux_list = []
     randonlength = random.randint(1, 100)
-
     for i in range(randonlength):
         aux_list.append(randonlength)
         i += i
     return aux_list
-
 my_stupid_list = generate_random_list()
-
 # Write your code below this comment, good luck!
-
+list_len = len(my_stupid_list)
+the_last_one = my_stupid_list[list_len -1]
+print(the_last_one)
