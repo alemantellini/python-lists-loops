b3460eae6571ab949e51eeedd31d559518140fab
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:30:01 +0100

    Do while

--- exercises/07-Do_while/app.py
+++ exercises/07-Do_while/app.py
@@ -1,1 +1,11 @@
+# EJERCICIO 7 - do while
 # Your code here
+i = 20
+while i >= 1:
+    if i % 5 == 0:
+        print(str(i) + "!")
+    else:
+        print(i)
+    i -= 1
+else:
+    print("LIFTOFF")
