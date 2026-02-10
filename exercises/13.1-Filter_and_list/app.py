86dd9a0f3bdcd9a1242fc92a0e6a006f6ff414f2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:38:43 +0100

    Filter and list

--- exercises/13.1-Filter_and_list/app.py
+++ exercises/13.1-Filter_and_list/app.py
@@ -1,9 +1,8 @@
+# EJERCICIO 13.1 - filter and list
 all_names = ["Romario", "Boby", "Roosevelt", "Emiliy", "Michael", "Greta", "Patricia", "Danzalee"]
-
 # Your code here
-
+def filter_function(name):
+    # Update here
+    return name.startswith("R")   
+resulting_names = list(filter(filter_function, all_names))
 print(resulting_names)
-
-
-
-
