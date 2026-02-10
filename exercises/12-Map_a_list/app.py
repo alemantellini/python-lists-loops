ba8678763beb94f154f7c9bc5838e4c70857863d
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:34:45 +0100

    Map a list

--- exercises/12-Map_a_list/app.py
+++ exercises/12-Map_a_list/app.py
@@ -1,9 +1,8 @@
+# EJERCICIO 12 - map a list
 celsius_values = [-2, 34, 56, -10]
-
 def celsius_to_fahrenheit(celsius):
     # The magic happens here
-   
-
+    total = (celsius * 9/5) + 32
+    return total 
 result = list(map(celsius_to_fahrenheit, celsius_values))
-
 print(result)
