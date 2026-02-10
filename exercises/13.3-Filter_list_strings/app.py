51dcc8a4e0ee43dba337fb88e5625c11466c23c4
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:40:13 +0100

    Filter list strings

--- exercises/13.3-Filter_list_strings/app.py
+++ exercises/13.3-Filter_list_strings/app.py
@@ -1,3 +1,7 @@
+# EJERCICIO 13.3 - filter list strings
 names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Logan','Sophia','Benjamin','Mia','Mason','Charlotte','Elijah','Amelia','Oliver','Evelyn','Jacob','Abigail','Lucas','Harper','Michael','Emily','Alexander','Elizabeth','Ethan','Avery','Daniel','Sofia','Matthew','Ella','Aiden','Madison','Henry','Scarlett','Joseph','Victoria','Jackson','Aria','Samuel','Grace','Sebastian','Chloe','David','Camila','Carter','Penelope','Wyatt','Riley']
-
 # Your code here
+def filter_function(name):
+    return "am" in name.lower()
+resulting_names = list(filter(filter_function, names))
+print(resulting_names)
