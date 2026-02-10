750478b5d02221d48fec31b5ac2916afb69564e2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:28:04 +0100

    Sum odd items

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,3 +1,10 @@
+# EJERCICIO 5.1 - sum odd items
 my_list = [4,5,734,43,45,100,4,56,23,67,23,58,45]
-
 # Your code here
+def sum_odds(list):
+    total = 0
+    for x in range(len(list)):
+        if list[x] % 2 != 0:
+            total += list[x]
+    return total
+print(sum_odds(my_list))
