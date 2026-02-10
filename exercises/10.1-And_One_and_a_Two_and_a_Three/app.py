7c61bfda86e77b41620ccb6b27175662aaaaf8c4
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:33:44 +0100

    And one and a two and a three

--- exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
+++ exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
@@ -1,8 +1,12 @@
+# EJERCICIO 10.1 - and one and a two and a three
 contact = {
     "fullname": "Jane Doe",
     "phone": "321-321-4321",
     "email": "test@test.com"
 }
-
 # Your code here
-
+keys = contact.keys()
+values = contact.values()
+items = contact.items()
+for i in items:
+    print(i[0]+":"+" "+i[1])
