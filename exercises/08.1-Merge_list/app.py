d173da4147d00a5e71954f29e20414a9529994e1
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:31:22 +0100

    Merge list

--- exercises/08.1-Merge_list/app.py
+++ exercises/08.1-Merge_list/app.py
@@ -1,9 +1,12 @@
+# EJERCICIO 8.1 - merge list
 chunk_one = ['Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell']
 chunk_two = ['Lucas', 'Jake', 'Scott', 'Amy', 'Molly', 'Hannah', 'Lucas']
-
-
 def merge_list(list1, list2):
     # Your code here
-
-    
+    new_list = []
+    for i in list1:
+        new_list.append(i)
+    for x in list2:
+        new_list.append(x)
+    return new_list   
 print(merge_list(chunk_one, chunk_two))
