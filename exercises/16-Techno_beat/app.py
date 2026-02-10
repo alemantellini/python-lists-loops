f0c118f8bace9f4fec4de28e179cd1ce014b03b2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:48:15 +0100

    Techno beat

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,4 +1,19 @@
+# EJERCICIO 16 - techno beat
+def lyrics_generator(numbers):
+    song = ""
+    count_ones = 0
+    for i in numbers:
+        if i == 0:
+            song += "Boom "
+            count_ones = 0
+        elif i == 1:
+            song += "Drop the bass "
+            count_ones += 1
 
+            if count_ones == 3:
+                song += "!!!Break the bass!!! "
+                count_ones = 0
+    return song
 
 # Your code above, nothing to change after this line
 print(lyrics_generator([0,0,1,1,0,0,0]))
