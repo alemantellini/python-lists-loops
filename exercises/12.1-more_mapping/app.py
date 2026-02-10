240e006b0d256c2956c807d53d258498a2917302
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:35:12 +0100

    More mapping

--- exercises/12.1-more_mapping/app.py
+++ exercises/12.1-more_mapping/app.py
@@ -1,5 +1,8 @@
+# EJERCICIO 12.1 - more mapping
 my_numbers = [23,234,345,4356234,243,43,56,2]
-
 # Your code here
-
+def multiply_by_three(number):
+    total = number * 3
+    return total 
+new_list = list(map(multiply_by_three, my_numbers))
 print(new_list)
