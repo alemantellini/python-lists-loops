f63babf511896e0684512563dcd7d7a7212ad321
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:35:37 +0100

    Map function inside variable

--- exercises/12.2-Map_function_inside_variable/app.py
+++ exercises/12.2-Map_function_inside_variable/app.py
@@ -1,6 +1,7 @@
+# EJERCICIO 12.2 - map function inside variable
 names = ['Alice', 'Bob', 'Marry', 'Joe', 'Hilary', 'Stevia', 'Dylan']
-
 def prepender(name):
     return "My name is: " + name
-    
 # Your code here
+new_list = list(map(prepender, names))
+print(new_list)
