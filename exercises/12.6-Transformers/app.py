204497898c706d3af47a9b4328fb3087282fb1b2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:37:50 +0100

    Transformers

--- exercises/12.6-Transformers/app.py
+++ exercises/12.6-Transformers/app.py
@@ -1,3 +1,4 @@
+# EJERCICIO 12.6 - Transformers
 incoming_ajax_data = [
     { "name": 'Mario', "last_name": 'Montes' },
     { "name": 'Joe', "last_name": 'Biden' },
@@ -7,4 +8,10 @@ incoming_ajax_data = [
 ]
 
 # Your code here
+def full_name(person):
+    return person["name"] + " " + person["last_name"]
 
+def data_transformer(strings):
+    return list(map(full_name, strings))
+
+print(data_transformer(incoming_ajax_data))
