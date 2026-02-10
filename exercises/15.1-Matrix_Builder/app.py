6594f2d4ab66f83d7ec17cb2da012fd14a26edef
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:47:13 +0100

    Matrix builder

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,1 +1,11 @@
+# EJERCICIO 15.1 - matrix builder
 # Your code here
+def matrix_builder(integer):
+    result = []
+    for i in range(integer):
+        row = []
+        for x in range(integer):
+            row.append(1)
+        result.append(row)
+    return result
+print(matrix_builder(3))
