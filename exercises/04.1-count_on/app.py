ae70a9beaa371432a9f1a50c5504f84fd4148d7c
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:26:18 +0100

    Count on

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,3 +1,9 @@
+# EJERCICIO 4.1 - count on
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
-
 # Your code here
+new_list = []
+for x in range(len(my_list)):
+    list_type = type(my_list[x])
+    if list_type is list or list_type is dict:
+        new_list.append(my_list[x])
+print(new_list)
