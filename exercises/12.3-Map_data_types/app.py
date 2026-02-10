4e6d441cc8878c6ba3779a2ae4245b05d9777c30
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:35:59 +0100

    Map data types

--- exercises/12.3-Map_data_types/app.py
+++ exercises/12.3-Map_data_types/app.py
@@ -1,9 +1,7 @@
+# EJERCICIO 12.3 - map data types
 mixed_list = ['1','5','45','34','343','34',6556,323]
-
 def type_list(items):
         # Your code here
-        return 
-
+        return type(items)
 new_list = list(map(type_list, mixed_list))
-
 print(new_list)
