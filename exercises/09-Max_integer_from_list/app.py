5f922dad2196a4df546a2c6a995a85c51fa061ed
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:32:22 +0100

    Max integer from list

--- exercises/09-Max_integer_from_list/app.py
+++ exercises/09-Max_integer_from_list/app.py
@@ -1,3 +1,10 @@
+# EJERCICIO 9 - max integer from list
 my_list = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34]
-
 # Your code here
+def max_integer(list):
+    result = 0
+    for i in list:
+        if i > result:
+            result = i
+    return result
+print(max_integer(my_list))
