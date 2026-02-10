7b3360193f77ba6325edd6381b7b94876ae8c9cf
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:34:11 +0100

    Nested list

--- exercises/11-Nested_list/app.py
+++ exercises/11-Nested_list/app.py
@@ -1,3 +1,5 @@
+# EJERCICIO 11 - nested list
 coordinates_list = [[33.747252, -112.633853], [-33.867886, -63.987], [41.303921, -81.901693], [-33.350534, -71.653268]]
-
 # Your code here
+for long in coordinates_list:
+    print(long[1])
