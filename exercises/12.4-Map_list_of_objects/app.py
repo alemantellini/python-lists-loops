45490cd02009352309b1300c9d37d5b2002fc085
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:36:32 +0100

    Map list of objects

--- exercises/12.4-Map_list_of_objects/app.py
+++ exercises/12.4-Map_list_of_objects/app.py
@@ -1,5 +1,5 @@
+# EJERCICIO 12.4 - map list of objects
 import datetime
-
 people = [
 	{ "name": 'Joe', "birth_date": datetime.datetime(1986,10,24) },
 	{ "name": 'Bob', "birth_date": datetime.datetime(1975,5,24) },
@@ -7,17 +7,13 @@ people = [
 	{ "name": 'Dylan', "birth_date": datetime.datetime(1999,12,14) },
 	{ "name": 'Steve', "birth_date": datetime.datetime(2003,4,24) }
 ]
-
 def calculate_age(date_of_birth):
     today = datetime.date.today()
     age = today.year - date_of_birth.year - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
     return age
-
 def format_greeting(person):
     # Your code here
-    return person["name"]
-
-
+    age = calculate_age(person["birth_date"])
+    return "Hello, my name is " + person["name"] + " and I am " + str(age) + " years old"
 name_list = list(map(format_greeting, people))
-
 print(name_list)
