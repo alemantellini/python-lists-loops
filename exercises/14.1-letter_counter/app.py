153aae492ebd8401ed07e8a41c3fefeade6c55c8
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:44:27 +0100

    Letter counter

--- exercises/14.1-letter_counter/app.py
+++ exercises/14.1-letter_counter/app.py
@@ -1,8 +1,12 @@
+# EJERCICIO 14.1 - letter counter
 par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
-
 counts = {}
-
 # Your code here
-
-
+for letter in par:
+    if letter != " ":
+        lower_letter = letter.lower()
+        if lower_letter in counts:
+            counts[lower_letter] += 1
+        else:
+            counts[lower_letter] = 1
 print(counts)
