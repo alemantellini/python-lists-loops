214a3a052f991bb08d9bf2d71689290032e72575
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:25:48 +0100

    Mixed list

--- exercises/04-mixed_list/app.py
+++ exercises/04-mixed_list/app.py
@@ -1,3 +1,8 @@
+# EJERCICIO 4 - mixed list
 mix = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
-
 # Your code below
+def values(list):
+    for x in range(len(list)):
+        mix_type = type(list[x])
+        print(mix_type)
+values(mix)
