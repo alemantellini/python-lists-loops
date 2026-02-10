38f91a8ac94ea23d9a2f0ac1622dd0ce21eac3d2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:36:56 +0100

    Yes and No

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,4 +1,10 @@
+# EJERCICIO 12.5 - yes and no
 the_bools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
-
 # Your code here
-
+def wikiwoko(element):
+    if element == 1:
+        return "wiki"
+    else:
+        return "woko"
+my_list = list(map(wikiwoko, the_bools))
+print(my_list)
