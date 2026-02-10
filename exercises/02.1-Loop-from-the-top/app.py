764952389eff9e0ec4e8e9b39b079d8aca35c8af
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:22:25 +0100

    Loop from the top

--- exercises/02.1-Loop-from-the-top/app.py
+++ exercises/02.1-Loop-from-the-top/app.py
@@ -1,6 +1,5 @@
+# EJERCICIO 2.1 - loop from the top
 my_sample_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
-
 # Modify the loop below to print from end to start
-
-for i in range(0, len(my_sample_list)):
+for i in range(len(my_sample_list)-1, -1, -1):
     print(my_sample_list[i])
