6b1ffaa88c79717e18144d04fe2db7f611d5c670
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:30:34 +0100

    Delete element

--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -1,11 +1,14 @@
+# EJERCICIO 8 - delete element
 people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
-
 def delete_person(person_name):
     # Your code here
-    
-
-    
+    new_list = []
+    for i in people:
+        if i != person_name:
+            new_list.append(i)
+    return new_list
 # Don't delete anything below
 print(delete_person("daniella"))
 print(delete_person("juan"))
 print(delete_person("emilio"))
+print(delete_person("ana"))
