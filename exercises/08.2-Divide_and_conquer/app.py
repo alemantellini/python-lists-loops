b0536e26b2727b2ebe6a39360c398b4201413390
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:31:48 +0100

    Divide and conquer

--- exercises/08.2-Divide_and_conquer/app.py
+++ exercises/08.2-Divide_and_conquer/app.py
@@ -1,7 +1,14 @@
+# EJERCICIO 8.2 - divide and conquer
 list_of_numbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68]
-
 # Your code here
-
-
+def sort_odd_even(integers):
+    sorted_list = []
+    even = []
+    for i in integers:
+        if i % 2 == 0:
+            even.append(i)
+        else:
+            sorted_list.append(i)
+    sorted_list.extend(even)
+    return sorted_list
 print(sort_odd_even(list_of_numbers))
-
